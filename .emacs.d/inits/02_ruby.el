69c1fd67a0425549ba825b5ae1d12f91218b200e
k0uki <k0uki221@gmail.com>
2013-04-08 00:10:45 +0900

    add feature-mode,alias for rails and cake

--- .emacs.d/inits/02_ruby.el
+++ .emacs.d/inits/02_ruby.el
@@ -23,3 +23,6 @@
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+;; cucumber
+(require 'feature-mode)
